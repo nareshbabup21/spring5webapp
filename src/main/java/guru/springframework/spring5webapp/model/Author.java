173d388b6c6a771d3d4a3568673a42e2e048304d
nareshbabup21 <nareshbabup21@gmail.com>
2019-11-28 10:22:00 -0500

    comments included in the author class

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -9,6 +9,7 @@ public class Author {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    //declaring variables
     private Long id;
     private String firstName;
     private String lastName;
